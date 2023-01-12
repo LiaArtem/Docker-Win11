707f7003f4abd90b4bf523b611751bb5e2e12e60
Artem Liakhovskyi <54625091+LiaArtem@users.noreply.github.com>
2023-01-12 13:22:49 +0200

    update scripts

--- MariaDB/!create_mariadb.bat
+++ MariaDB/!create_mariadb.bat
@@ -1,7 +1,7 @@
 cd %cd%
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './mariadb.ps1'"
 
-PowerShell -command "Start-Sleep -s 10"
+PowerShell -command "Start-Sleep -s 30"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './mariadb_sql.ps1'"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './mariadb_sql_object.ps1'"
 pause
