8a359ecd40521efcf72b2a695ba0abd74f073574
Artem Liakhovskyi <54625091+LiaArtem@users.noreply.github.com>
2023-07-08 20:45:05 +0300

    Update mariadb.ps1
    fix version mariadb

--- MariaDB/mariadb.ps1
+++ MariaDB/mariadb.ps1
@@ -1,4 +1,4 @@
-docker run --name MariaDBContainer --restart=always -p 3307:3306 -e MARIADB_ROOT_PASSWORD=!Aa112233 -d mariadb:latest
+docker run --name MariaDBContainer --restart=always -p 3307:3306 -e MARIADB_ROOT_PASSWORD=!Aa112233 -d mariadb:10.11.4
 
 
 
