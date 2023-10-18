89e3cf25a1b3ee503caec1971519fbaee697f4fd
Artem Liakhovskyi <54625091+LiaArtem@users.noreply.github.com>
2023-10-18 14:37:13 +0300

    Update sql_add_testdb.sql

--- PostgreSQL/sql_add_testdb.sql
+++ PostgreSQL/sql_add_testdb.sql
@@ -1343,6 +1343,94 @@ begin
 $function$
 ;
 
+CREATE OR REPLACE FUNCTION p_convert.str_days(p_value integer)
+ RETURNS character varying
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+DECLARE
+    -- Описание (дні)
+    result     varchar(255);
+    p_name_day varchar(255);
+    DayValue   integer;
+    CResult    varchar(20);
+    l          integer;
+begin	
+    DayValue := Trunc(p_value);
+    CResult := to_char(DayValue);
+    l := length(CResult);
+
+    if ((l>1) and (to_number(substr(CResult,l-1,2))>10) and (to_number(substr(CResult,l-1,2))<20))
+     then
+       p_name_day := ' днів ';
+    elsif to_number(substr(CResult,l,1))=0
+     then
+       p_name_day := ' днів ';
+    elsif to_number(substr(CResult,l,1))=1
+     then
+       p_name_day := ' день ';
+    elsif (to_number(substr(CResult,l,1))=2) or (to_number(substr(CResult,l,1))=3) or (to_number(substr(CResult,l,1))=4)
+     then
+       p_name_day:= ' дні ';
+    else
+       p_name_day := ' днів ';
+    end if;
+
+    Result := p_name_day;
+
+    return (trim(substr(Result, 1, 255)));
+  exception when others
+  then
+    return(Result);
+  end;              
+  
+$function$
+;
+
+CREATE OR REPLACE FUNCTION p_convert.str_month(p_value integer)
+ RETURNS character varying
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+DECLARE
+    -- Описание (місяці)
+    result       varchar(255);
+    p_name_month varchar(255);
+    MonthValue   integer;
+    CResult      varchar(20);
+    l            integer;
+  begin
+    MonthValue := Trunc(p_value);
+    CResult := to_char(MonthValue);
+    l := length(CResult);
+
+    if ((l>1) and (to_number(substr(CResult,l-1,2))>10) and (to_number(substr(CResult,l-1,2))<20))
+     then
+       p_name_month := ' місяців ';
+    elsif to_number(substr(CResult,l,1))=0
+     then
+       p_name_month := ' місяців ';
+    elsif to_number(substr(CResult,l,1))=1
+     then
+       p_name_month := ' місяць ';
+    elsif (to_number(substr(CResult,l,1))=2) or (to_number(substr(CResult,l,1))=3) or (to_number(substr(CResult,l,1))=4)
+     then
+       p_name_month:= ' місяці ';
+    else
+       p_name_month := ' місяців ';
+    end if;
+
+    Result := p_name_month;
+
+    return (trim(substr(Result, 1, 255)));
+  exception when others 
+  then 
+    return(Result);
+  end;           
+ 
+$function$
+;
+
 -----------------------------------------------------
 -- DROP SCHEMA p_interface;
 
